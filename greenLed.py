0eb8bf5238963356be1da1c42296e78c9c79e1ed
David Isaak <disaak@sixbluedata.net>
2017-08-12 06:35:10 -0700

    added sleep function

--- greenLed.py
+++ greenLed.py
@@ -1,10 +1,11 @@
 import RPi.GPIO as GPIO
-#import time
+import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(16, GPIO.OUT)
 
-
-GPIO.output(16, GPIO.HIGH)
-
-GPIO.cleanup()
+while True:
+    GPIO.output(16, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(16, GPIO.LOW)
+    time.sleep(1)
